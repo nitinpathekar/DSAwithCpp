5c28c3030421447df551592ad2db76644e671918
Nitin <nitinsardarpathekar@gmail.com>
2024-07-09 14:30:52 +0530

    this is 1st

--- pattern.cpp
+++ pattern.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 using namespace std;
 int main(){
-    this is 
 
+    int n;
+    cin>>n;
+    for (int i=1; i<=n;i++){
+        for(int j=1; j<=n; j++){
+            char c='A'+i+j-2;
+            cout<<c;
+        }
+        cout<<"\n";
+    }
     return 0;
 }
