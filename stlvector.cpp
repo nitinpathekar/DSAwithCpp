d004cd8751902564702d2266ca04a22abeaf9ebf
Nitin <nitinsardarpathekar@gmail.com>
2024-07-10 22:50:34 +0530

    add some code

---
+++ stlvector.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int> v;
+    cout<<"Capacity: "<<v.capacity()<<endl;
+    v.push_back(1);
+
+    cout<<"Capacity: "<<v.capacity()<<endl;
+    v.push_back(2);    cout<<"Capacity: "<<v.capacity()<<endl;
+    v.push_back(1);
+
+    cout<<"Capacity: "<<v.capacity()<<endl;
+    v.push_back(2);
+    
+    return 0;
+}
