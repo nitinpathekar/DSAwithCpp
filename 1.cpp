c135957d177bb4afa6994ab2097a20567210a657
Nitin Pathekar <nitinsardarpathekar@gmail.com>
2024-10-28 20:35:58 +0530

    new add

--- 1.cpp
+++ 1.cpp
@@ -1,6 +1,28 @@
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
+
+struct Pair{
+    int min;
+    int max;
+};
+Pair getminmax(int arr[], int n){
+    sort(arr, arr+n);
+    Pair minmax;
+    
+    minmax.min=arr[0];
+    minmax.max=arr[n-1];
+    return minmax;
+
+}
+
+
 int main(){
-    cout<<"HEllo";
+    int arr[] = { 1000, 11, 445, 1, 330, 3000 };
+    int arr_size = sizeof(arr)/sizeof(arr[0]);
+    Pair minmax = getminmax(arr,arr_size);
+
+    cout << "Minimum element is " << minmax.min << endl;
+    cout << "Maximum element is " << minmax.max << endl;
+
     return 0;
 }
